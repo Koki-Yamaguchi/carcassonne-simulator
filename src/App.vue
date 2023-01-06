62af4f602704f82e9e8764fb9032dec261c6c9d4
Koki Yamaguchi <yk49at21@gmail.com>
2023-01-07 08:28:09 +0900

    Add

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
